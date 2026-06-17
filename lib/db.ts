9d99451bcf5c344e7cc77e8a454f482a77d4322c
sidharth-webdev <shidharthpradhan999@gmail.com>
2026-06-17 18:05:09 +0530

    updated db file

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 const MONGODB_URI = process.env.MONGODB_URI! 
 
 if(!MONGODB_URI){
@@ -12,3 +13,29 @@ if(!cached){
     cached = global.mongoose = {conn: null, promise: null}
 }
 
+export async function connectToDatabase(){
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+
+    const opts = {
+        bufferCommands: true,
+        maxPoolSize: 10
+    }
+
+        mongoose
+        .connect(MONGODB_URI, opts)
+        .then(()=> mongoose.connection)
+    }
+
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null
+        throw error
+    }
+
+    return cached.conn
+}           
