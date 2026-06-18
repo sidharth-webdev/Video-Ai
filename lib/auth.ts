2bdcd9597ee205fccef440f48fea9a9235added0
sidharth-webdev <shidharthpradhan999@gmail.com>
2026-06-19 03:17:08 +0530

    update in lib

--- lib/auth.ts
+++ lib/auth.ts
@@ -71,5 +71,5 @@ export const authOptions: NextAuthOptions = {
     strategy:"jwt",
     maxAge: 30 * 24 * 60 * 60,
   }, 
-  secret: process.env.NEXTAUTH_SECRET,
-};        
+  secret: process.env.NEXTAUTH_SECRET, 
+};         
